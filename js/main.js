12f709cf44096b6e14e6a1b9cd63cb523ed3faad
vomelnit <womelnitsky@gmail.com>
2019-05-29 08:33:34 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$(".fancybox").fancybox({
+openEffect: "none",
+closeEffect: "none"
+});
+$(".zoom").hover(function(){
+$(this).addClass('transition');}, function(){
+$(this).removeClass('transition');
+});
+});
